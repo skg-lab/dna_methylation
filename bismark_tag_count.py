0187c217bac484b5c4c82d9472e427585cca4529
ykohki <43834721+ykohki@users.noreply.github.com>
2019-03-10 12:08:08 +0900

    `df_tag_bed_list=[] df_tag_bed_list.append(tag_bed) all_tag_bed=pd.concat(df_tag_bed_list) all_tag_bed.to_csv(out_put_file, mode='a',sep='\t',index=False)` にて、concatで結合するようにしました。
    変数の名前も少し変更しました。
    range(len())の部分は無くすとエラーが出るので、とりあえずつけておきます。iterrowsと合わせてもう一度見てみます。

--- bismark_tag_count.py
+++ bismark_tag_count.py
@@ -14,23 +14,24 @@ df_bismark_drop=df_bismark.drop(drop_row, axis=1)
 df_bed = pd.read_csv(input_bed_file, sep='\t',names=('chr','start','end'))
 
 
-df_bed_list=[]
+list_df_bed=[]
 df_bismark_list=[]
 
-chr_list=['chr1','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19',
+list_chr=['chr1','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19',
 'chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chrM','chrX','chrY']
 
-for i in range(len(chr_list)):
-    df_bed_chr=df_bed[df_bed['chr'] == chr_list[i]]
-    df_bed_list.append(df_bed_chr)
+for i in range(len(list_chr)):
+    df_bed_chr=df_bed[df_bed['chr'] == list_chr[i]]
+    list_df_bed.append(df_bed_chr)
 
-for i in range(len(chr_list)):
-    df_bismark_chr=df_bismark_drop[df_bismark_drop['chr'] == chr_list[i]]
+for i in range(len(list_chr)):
+    df_bismark_chr=df_bismark_drop[df_bismark_drop['chr'] == list_chr[i]]
     df_bismark_list.append(df_bismark_chr)
 
+df_tag_bed_list=[]
 
-for i in range(len(chr_list)):
-    bed=df_bed_list[i]
+for i in range(len(list_chr)):
+    bed=list_df_bed[i]
     bismark=df_bismark_list[i]
     bed_list=bed.values.tolist()
 
@@ -38,10 +39,11 @@ for i in range(len(chr_list)):
         x=bed_list[j][1]
         y=bed_list[j][2]
 
-        bismark_query=bismark.query("@x<= start<=@y",engine='numexpr')
+        bismark_query=bismark.query("@x<= start<=@y")
         bed_list[j].append(bismark_query['methyl'].sum())
         bed_list[j].append(bismark_query['de_methyl'].sum())
+    tag_bed = pd.DataFrame(bed_list,columns=('chr','start','end','methyl_sum','de_methyl_sum'))
+    df_tag_bed_list.append(tag_bed)
 
-    tag_bed = pd.DataFrame(bed_list,index=False,,columns=('chr','start','end','methyl_sum','de_methyl_sum'))
-
-    tag_bed.to_csv(out_put_file, mode='a',sep='\t',index=False)
+all_tag_bed=pd.concat(df_tag_bed_list)
+all_tag_bed.to_csv(out_put_file, mode='a',sep='\t',index=False)
